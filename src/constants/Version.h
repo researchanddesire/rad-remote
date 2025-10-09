06c8d81d9caf5fb01e0d31dde2171cb01867952d
AJ-Koenig <39343698+AJ-Koenig@users.noreply.github.com>
2025-10-09 17:47:45 +0000

    AUTO: Version bump 1.0.8

--- src/constants/Version.h
+++ src/constants/Version.h
@@ -1,9 +1,9 @@
 #ifndef OSSM_REMOTE_VERSION_H
 #define OSSM_REMOTE_VERSION_H
 
-#define VERSION "1.0.7"
+#define VERSION "1.0.8"
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 0
-#define PATCH_VERSION 7
+#define PATCH_VERSION 8
 
 #endif  // OSSM_REMOTE_VERSION_H
